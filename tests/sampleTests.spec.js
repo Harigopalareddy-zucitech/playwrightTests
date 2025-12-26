f162a1efb044e6c0d7a34f3546403e31d6c9d7b2
Harigopalareddy-zucitech <hari.tetala@zucitech.com>
2025-12-26 12:21:50 +0530

    changed to flipkart site

--- tests/sampleTests.spec.js
+++ tests/sampleTests.spec.js
@@ -1,7 +1,8 @@
 import { test } from 'playwright/test';
 
 test('sample test', async ({ page }) => {
-    await page.goto("https://practicetestautomation.com/practice")
-    await page.locator('//a[text()="Test Login Page"]').waitFor({ state: 'visible' })
-    await page.locator('//a[text()="Test Login Page"]').click()
-})
+    await page.goto("https://flipkart.com")
+    await page.locator("//span[text()='Login']").hover()
+    await page.locator("//span[text()='Sign Up']").click()
+    await page.waitForTimeout(10000)
+})   
